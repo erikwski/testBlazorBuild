624c8ccbfef811a1d21ac13bc6e32c6e70bf9c12
Erik Ferrari <eferrari@datacolor.com>
2024-07-01 15:52:03 +0200

    First commit

---
+++ MyStaticClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+
+namespace blazorBridge
+{
+    public static class MyStaticClass
+    {   
+
+        [JSInvokableAttribute("MyClass.checkDotNetLoaded")]
+        public static string getMessage2()
+        {
+            return "Hello from DotNet!";
+        }
+
+        [JSInvokableAttribute("MyClass.addNumbers")]
+        public static int addNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
